881c096589a181e8c616f609c4cc517d3967c521
Artem3423 <art20032345@gmail.com>
2021-12-16 09:51:26 +0300

    Initial commit

---
+++ app/src/main/java/com/example/alculator/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.alculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.textcalc.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
